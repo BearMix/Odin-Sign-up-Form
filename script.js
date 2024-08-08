028d4b23f04bae860dc322e87271b0872d0dd1b7
BearMix <mix8704@gmail.com>
2024-08-09 00:01:49 +0300

    Start

---
+++ script.js
@@ -0,0 +1,7 @@
+const submitBtn = document.querySelector('submit-btn');
+const phone = document.querySelector('#phone');
+const password = document.querySelector('#password');
+const passwordConfirm = document.querySelector('#password-confirm');
+const email = document.querySelector('#email');
+const errorDisplayers = document.getElementsByClassName('error');
+const inputFields = document.querySelectorAll('input');
